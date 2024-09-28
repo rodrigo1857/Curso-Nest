133c231147c97c3149f3719ac6cc13cbc742f6b5
rodrigo1857 <rodrigo.vega@unmsm.edu.pe>
2024-09-27 23:52:56 -0500

    feat(pokemon) : crea custom pipe

--- 03-pokedex/src/pokemon/pokemon.service.ts
+++ 03-pokedex/src/pokemon/pokemon.service.ts
@@ -70,10 +70,12 @@ try {
       }
     }
   async remove(id: string) {
-    const pokemon = await this.findOne(id);
-    await pokemon.deleteOne();
-    //this.pokemonModel.findByIdAndDelete(id);
-    //return `This action removes a #${id} pokemon`;
+    // const pokemon = await this.findOne(id);
+    // await pokemon.deleteOne();
+    //const result = await this.pokemonModel.findByIdAndDelete(id);
+    const {deletedCount} = await this.pokemonModel.deleteOne({_id: id});
+    if(deletedCount === 0) throw new BadRequestException(`Pokemon not found`);
+    return ; 
   }
 
 
